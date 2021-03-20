4caca09d44d4c773a15f849fdf14520ab4e0781d
Taylor Griffin <titleist315@gmail.com>
2021-03-19 20:24:40 -0500

    fruit loops 1 done

---
+++ CSF1Homework/FruitLoops1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoops1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fruity loops");
+            string[] fruit = { "apple", "banana", "orange", "kiwi", "melon", "grapefruit" };
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.WriteLine(fruit[i]);
+
+            }
+            
+        }
+    }
+}
