e6b989fd289a20f0a2124de30a234eb97084c841
Taylor Griffin <titleist315@gmail.com>
2021-03-19 21:14:52 -0500

    fruit loops 2 done

---
+++ CSF1Homework/FruitLoops2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoops2
+    {
+        static void Main()
+        {
+            string[] fruit = { "apple", "banana", "orange", "kiwi", "melon", "grapefruit" };
+            string[] period = { ".", "..", "...", "....", ".....", "......" };
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                
+                Console.WriteLine(fruit[i]);
+                Console.WriteLine(period[i]);
+                    
+                    
+                    
+            }
+        }
+    }
+}
