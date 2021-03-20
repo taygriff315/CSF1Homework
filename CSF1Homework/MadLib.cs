e229e9447f93b13d65decb054542be566f049576
Taylor Griffin <titleist315@gmail.com>
2021-03-20 11:08:46 -0500

    mad lib done

---
+++ CSF1Homework/MadLib.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class MadLib
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, enter a noun");
+            string noun = Console.ReadLine();
+            Console.WriteLine("Please, enter a verb");
+            string verb = Console.ReadLine();
+            Console.WriteLine("Please, enter an adjective");
+            string adj = Console.ReadLine();
+            Console.WriteLine("Please, enter a noun");
+            string nounTwo = Console.ReadLine();
+            Console.WriteLine("Please, enter a verb");
+            string verbTwo = Console.ReadLine();
+            Console.WriteLine("Please, enter a noun");
+            string nounThree = Console.ReadLine();
+            Console.Clear();
+
+
+            Console.WriteLine($"There once was a {noun}. They {verb} everyday and dreamed that they would one day be {adj}. Things don't always go the way they seem tho and one day " +
+                $"a {nounTwo} came and took everthing they had {verbTwo} for. This is why you should never trust a {nounThree}....") ;
+        }
+    }
+}
